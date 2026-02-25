e69b187c553f04a0de4e74fcb81a31f76c1fc830
Peonie lin <98808956+peonielin@users.noreply.github.com>
2026-02-25 17:19:01 +1100

    exercises 1-6

--- javascript/javascript-exercises-main/foundations/06_repeatString/repeatString.js
+++ javascript/javascript-exercises-main/foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+    if (number <0)
+    return "ERROR";
+
+
+    let output ="";
+
+if (number >= 0){
+
+        for (let step =0; step < number; step++){
+            output += string ;
+            
+        }
+ return output;
 };
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
