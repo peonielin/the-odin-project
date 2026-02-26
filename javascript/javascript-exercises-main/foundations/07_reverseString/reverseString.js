8f8ac8723ec445c786ccb68d506f7bad06d9e0d9
peonielin <98808956+peonielin@users.noreply.github.com>
2026-02-27 10:02:22 +1100

    Update reverseString.js

--- javascript/javascript-exercises-main/foundations/07_reverseString/reverseString.js
+++ javascript/javascript-exercises-main/foundations/07_reverseString/reverseString.js
@@ -2,9 +2,12 @@ const reverseString = function(str) {
 
   let newstr = "";
 
-  for (let i = str.length; i < str.length; i--) {
-    newstr += str[i]
-};
+  for (let i = str.length-1; i <=0; i--) {
+    newstr += str[i] 
+  };
 
+  return newstr;
+
+}
 // Do not edit below this line
 module.exports = reverseString;
