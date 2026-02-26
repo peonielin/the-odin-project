24f958659f9b2f4101eddaf016410cfd5060cdf4
peonielin <98808956+peonielin@users.noreply.github.com>
2026-02-27 10:00:28 +1100

    Update reverseString function to take a parameter
    Modify reverseString function to accept a string parameter and correctly reverse it.

--- javascript/javascript-exercises-main/foundations/07_reverseString/reverseString.js
+++ javascript/javascript-exercises-main/foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+  let newstr = "";
+
+  for (let i = str.length; i < str.length; i--) {
+    newstr += str[i]
 };
 
 // Do not edit below this line
